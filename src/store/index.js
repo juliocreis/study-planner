82159c0c34e50f93f37b8adcfe8c88fd05b39a7a
juliocreis <julioreis951@gmail.com>
2026-08-27 10:38:09 -0300

    feat: add themeSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from './slices/taskSlice'
+import themeReducer from './slices/themeSlice'
 
 export const store = configureStore({
   reducer: {
     tasks: taskReducer,
+    theme: themeReducer
   },
 })
 
