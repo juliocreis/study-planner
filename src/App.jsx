aa0aa8377321e6b5a69c185544fd10079fc0d507
juliocreis <julioreis951@gmail.com>
2026-09-01 14:20:33 -0300

    feat: create Store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { TaskProvider } from './contexts/TaskContext'
 import { ThemeProvider } from './contexts/ThemeContext'
 import StudyPlannerPage from './pages/StudyPlannerPage'
+import store from './store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <ThemeProvider>
-      <TaskProvider>
-        <StudyPlannerPage />
-      </TaskProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <TaskProvider>
+          <StudyPlannerPage />
+        </TaskProvider>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
